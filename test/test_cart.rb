17e34379c40a77ede5b88ae19a6ddd01bf70333f
maksim demin <mdemin914@gmail.com>
2012-09-06 15:09:05 -0700

    added tests that fail for cart item

--- test/test_cart.rb
+++ test/test_cart.rb
@@ -4,7 +4,13 @@ require 'helper'
 class CartTest < Test::Unit::TestCase
 	include ZillaBackendTestHelper
 
+	def test_create_new_cart
+		actually = ZillaBackend::Cart.new
+		assert_not_equal actually, nil
+	end
 
-
-
+	def test_cart_item
+		actually = ZillaBackend::Models.CartItem.new
+		assert_not_equal actually, nil
+	end
 end
